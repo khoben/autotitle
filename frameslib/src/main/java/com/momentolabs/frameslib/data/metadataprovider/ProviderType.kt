8e93baaba575f81c4f828d4fccf15364ad4f041f
khoben <extless@gmail.com>
2020-10-31 16:50:02 +0700

    Delete unused module

--- frameslib/src/main/java/com/momentolabs/frameslib/data/metadataprovider/ProviderType.kt
+++
@@ -1,6 +0,0 @@
-package com.momentolabs.frameslib.data.metadataprovider
-
-enum class ProviderType {
-    FFMPEG,
-    NATIVE_ANDROID
-}
--- frameslib/src/main/java/com/momentolabs/frameslib/data/model/Status.kt
+++
@@ -1,5 +0,0 @@
-package com.momentolabs.frameslib.data.model
-
-enum class Status {
-    EMPTY_FRAMES, LOADING, COMPLETED
-}
