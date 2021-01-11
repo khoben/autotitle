c152e5c4aca5cfefeb446588184a630cc05c11b0
khoben <extless@gmail.com>
2021-01-11 15:23:44 +0700

    Added opensource licenses dialog

---
+++ app/src/main/java/com/khoben/autotitle/ui/etc/OpenSourceLicensesDialog.kt
@@ -0,0 +1,33 @@
+package com.khoben.autotitle.ui.etc
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.os.Bundle
+import android.webkit.WebView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.marginTop
+import androidx.core.view.updateLayoutParams
+import androidx.fragment.app.DialogFragment
+import com.khoben.autotitle.extension.dp
+
+
+class OpenSourceLicensesDialog : DialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val webView = WebView(requireActivity()).apply {
+            settings.loadWithOverviewMode = true
+            settings.useWideViewPort = true
+            loadUrl("file:///android_asset/licenses.html")
+        }
+        return AlertDialog.Builder(requireActivity())
+            .setTitle("Open Source Licenses")
+            .setPositiveButton(android.R.string.ok) { dialog, _ -> dialog.dismiss() }
+            .create().apply {
+                setView(webView, 0, 15.dp(), 0, 0)
+            }
+    }
+
+
+    fun showLicenses(activity: AppCompatActivity) {
+        show(activity.supportFragmentManager, "dialog_licenses")
+    }
+}
