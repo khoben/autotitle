cb457db5e40808100d2b3533bdc82fc63d9b3dd8
khoben <extless@gmail.com>
2021-01-13 23:02:59 +0700

    LottieView Rotation property

--- app/src/main/java/com/khoben/autotitle/ui/etc/LottieAnimationViewEx.kt
+++ app/src/main/java/com/khoben/autotitle/ui/etc/LottieAnimationViewEx.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.util.AttributeSet
 import com.airbnb.lottie.LottieAnimationView
 import com.khoben.autotitle.R
+import timber.log.Timber
 
 class LottieAnimationViewEx(context: Context, attrs: AttributeSet?) :
         LottieAnimationView(context, attrs) {
@@ -15,11 +16,15 @@ class LottieAnimationViewEx(context: Context, attrs: AttributeSet?) :
             try {
                 val xmlMaxFrame = getInteger(R.styleable.LottieAnimationViewEx_lottie_maxFrame, 0)
                 val xmlMinFrame = getInteger(R.styleable.LottieAnimationViewEx_lottie_minFrame, 0)
+                val xmlRotation = getFloat(R.styleable.LottieAnimationViewEx_lottie_rotation, 0F)
                 if (xmlMaxFrame > 0) {
                     setMaxFrame(xmlMaxFrame)
                 }
                 if (xmlMinFrame > 0) {
                     setMinFrame(xmlMinFrame)
+                }
+                if (xmlRotation != 0F) {
+                    rotation = xmlRotation
                 }
             } finally {
                 recycle()
