9f48f90f5a435fde138639cfac745ceaa53c1ae0
khoben <extless@gmail.com>
2021-01-26 15:43:23 +0700

    WebView Dark mode

--- app/src/main/java/com/khoben/autotitle/ui/etc/OpenSourceLicensesDialog.kt
+++ app/src/main/java/com/khoben/autotitle/ui/etc/OpenSourceLicensesDialog.kt
@@ -2,10 +2,13 @@ package com.khoben.autotitle.ui.etc
 
 import android.app.AlertDialog
 import android.app.Dialog
+import android.content.res.Configuration
 import android.os.Bundle
 import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.DialogFragment
+import androidx.webkit.WebSettingsCompat
+import androidx.webkit.WebViewFeature
 import com.khoben.autotitle.extension.dp
 
 
@@ -15,6 +18,12 @@ class OpenSourceLicensesDialog : DialogFragment() {
             settings.loadWithOverviewMode = true
             settings.useWideViewPort = true
             loadUrl("file:///android_asset/licenses.html")
+        }
+        val nightModeFlags = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+        if (nightModeFlags == Configuration.UI_MODE_NIGHT_YES) {
+            if (WebViewFeature.isFeatureSupported(WebViewFeature.FORCE_DARK)) {
+                WebSettingsCompat.setForceDark(webView.settings, WebSettingsCompat.FORCE_DARK_ON)
+            }
         }
         return AlertDialog.Builder(requireActivity())
             .setTitle("Open Source Licenses")
